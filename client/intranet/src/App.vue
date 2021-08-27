d7e2a048bd74e9774e7c9d869c804e4607be2c52
Victor <victorcosta@gmail.com>
2021-08-27 14:35:50 +0100

    intranet

---
+++ client/intranet/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+   
+   <login/>
+  
+    <footer>
+      <h3>Desenvolvido por Victor Costa</h3>
+    </footer>
+  </div>
+</template>
+<script>
+  import login from './components/login.vue'
+export default {
+  
+  components:{
+    login
+  }
+
+}
+</script>
+
+<style>
+
+  footer{
+  
+    position: absolute;
+    bottom: 0;
+    text-align: center;
+    width: 100%;
+    height: 70px;
+    padding-top: 20px;
+    border-top: 2px solid orangered;
+  }
+</style>
