f3bcb3f81b043f54d528069ef8e90d99537c0176
Victor <victorcosta@gmail.com>
2021-09-02 20:50:11 +0100

    'intra'

--- client/intranet/src/router/index.js
+++ client/intranet/src/router/index.js
@@ -1,17 +1,28 @@
-import { createRouter, createWebHashHistory } from 'vue-router'
-import intranet from '../views/intranet.vue'
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Home from '../views/Home.vue'
+
+Vue.use(VueRouter)
 
 const routes = [
   {
-    path: '/sinoredi/intranet',
-    name: 'intranet',
-    component: intranet
+    path: '/',
+    name: 'Home',
+    component: Home
+  },
+  {
+    path: '/about',
+    name: 'About',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
   }
-  
 ]
 
-const router = createRouter({
-  history: createWebHashHistory(),
+const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
   routes
 })
 
